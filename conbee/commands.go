cf23bba6d29c2c439d43e40b2462a00c1580a3ba
Green Lightning <GreenLightning.git@googlemail.com>
2021-08-03 17:18:50 +0200

    Add parameter read/write commands

--- conbee/commands.go
+++ conbee/commands.go
@@ -17,6 +17,8 @@ func (a MACAddress) String() string {
 	return fmt.Sprintf("%016x", uint64(a))
 }
 
+// READ FIRMWARE VERSION
+
 type ReadFirmwareVersionRequest struct {
 	Reserved uint32
 }
@@ -49,6 +51,87 @@ func (r *ReadFirmwareVersionResponse) ParsePayload(data []byte) error {
 	return nil
 }
 
+// READ PARAMETER
+
+type ReadParameterRequest struct {
+	ParameterID NetParam
+}
+
+func init() {
+	registerParsable(outgoingParsables, CmdReadParameter, new(ReadParameterRequest))
+}
+
+func (r *ReadParameterRequest) ParsePayload(data []byte) error {
+	data, err := extractPayload(data, 1)
+	if err != nil {
+		return err
+	}
+	r.ParameterID = NetParam(data[0])
+	return nil
+}
+
+type ReadParameterResponse struct {
+	ParameterID NetParam
+	Parameter   []byte
+}
+
+func init() {
+	registerParsable(incomingParsables, CmdReadParameter, new(ReadParameterResponse))
+}
+
+func (r *ReadParameterResponse) ParsePayload(data []byte) error {
+	data, err := extractPayload(data, 0)
+	if err != nil {
+		return err
+	}
+	// @Note: Payload might be empty, if the parameter is not supported.
+	if len(data) != 0 {
+		r.ParameterID = NetParam(data[0])
+		r.Parameter = data[1:]
+	}
+	return nil
+}
+
+// WRITE PARAMETER
+
+type WriteParameterRequest struct {
+	ParameterID NetParam
+	Parameter   []byte
+}
+
+func init() {
+	registerParsable(outgoingParsables, CmdWriteParameter, new(WriteParameterRequest))
+}
+
+func (r *WriteParameterRequest) ParsePayload(data []byte) error {
+	data, err := extractPayload(data, 1)
+	if err != nil {
+		return err
+	}
+	r.ParameterID = NetParam(data[0])
+	r.Parameter = data[1:]
+	return nil
+}
+
+type WriteParameterResponse struct {
+	ParameterID NetParam
+}
+
+func init() {
+	registerParsable(incomingParsables, CmdWriteParameter, new(WriteParameterResponse))
+}
+
+func (r *WriteParameterResponse) ParsePayload(data []byte) error {
+	data, err := extractPayload(data, 1)
+	if err != nil {
+		return err
+	}
+	r.ParameterID = NetParam(data[0])
+	return nil
+}
+
+// DEVICE STATE
+
 type DeviceStateRequest struct{}
 
 func init() {
@@ -75,10 +158,12 @@ func (r *DeviceStateResponse) ParsePayload(data []byte) error {
 	return nil
 }
 
+// UPDATE NEIGHBOR
+
 type UpdateNeighborCommand struct {
-	Action byte
+	Action       byte
 	ShortAddress uint16
-	MACAddress MACAddress
+	MACAddress   MACAddress
 }
 
 func init() {
@@ -87,15 +172,25 @@ func init() {
 }
 
 func (c *UpdateNeighborCommand) ParsePayload(data []byte) error {
-	if len(data) < 13 {
-		return ErrInvalidPacket
+	data, err := extractPayload(data, 11)
+	if err != nil {
+		return err
+	}
+	c.Action = data[0]
+	c.ShortAddress = binary.LittleEndian.Uint16(data[1:3])
+	c.MACAddress = MACAddress(binary.LittleEndian.Uint64(data[3:11]))
+	return nil
+}
+
+// UTILITIES
+
+func extractPayload(data []byte, minimumPayloadLength int) ([]byte, error) {
+	if len(data) < 2+minimumPayloadLength {
+		return nil, ErrInvalidPacket
 	}
 	payloadLength := uint16(binary.LittleEndian.Uint16(data))
-	if int(payloadLength) + 2 != len(data) {
-		return ErrInvalidPacket
+	if len(data) != 2+int(payloadLength) {
+		return nil, ErrInvalidPacket
 	}
-	c.Action = data[2]
-	c.ShortAddress = binary.LittleEndian.Uint16(data[3:5])
-	c.MACAddress = MACAddress(binary.LittleEndian.Uint64(data[5:13]))
-	return nil
+	return data[2:], nil
 }
